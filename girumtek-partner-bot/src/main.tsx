dcb168cdb18d028cd5fc19ad62930c91cbf981fa
Girum <thegirumtekle@gmail.com>
2026-09-17 19:19:11 -0400

    feat: build chat form with Amplify UI components

--- girumtek-partner-bot/src/main.tsx
+++ girumtek-partner-bot/src/main.tsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import outputs from "../amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
 import { Amplify } from "aws-amplify";
+import { ThemeProvider } from "@aws-amplify/ui-react";
 
 Amplify.configure(outputs);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+  <ThemeProvider>
     <App />
-  </StrictMode>,
+  </ThemeProvider>
+</StrictMode>,
 )
